3dea5800c8d1119bfa096adf63aedb2efde4b0c7
davidDpruebas1 <74637140+davidDpruebas1@users.noreply.github.com>
2021-02-01 00:32:55 -0600

    Create IntUsuariosService.class.php

---
+++ service/IntUsuariosService.class.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	logica del negocio para la tabla usuarios
+*/
+	interface IntUsuariosService{
+		public function guardar($usuario);
+		public function buscarPorUsernamePassword($username,$password);
+
+		/*
+		
+		public function obtenerTodosusuarios();
+		public function eliminar($idUsuario);
+		public function modificar($usuario);
+
+		*/
+	}
+
+	?>
